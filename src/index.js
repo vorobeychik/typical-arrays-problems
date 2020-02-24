7e26336344d243d4b9ce62f4890bd381d158f562
vorobeychik <vorob.games@mail.ru>
2020-02-24 18:10:21 +0300

    refactor:add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(  array === undefined  || array.length === 0){return 0}
+    else{
+    let min = 100;
+    for(let i of array){
+        if( i < min){
+            min = i
+        }
+
+    }
+    return min;}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(  array === undefined  || array.length === 0){return 0}
+    let max = -100;
+    for(let i of array){
+        if( i > max){
+            max = i
+        }
+
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(  array === undefined  || array.length === 0){return 0}
+    let sum = 0;
+    for(let i of array){
+     sum += i;
+    }
+  return sum/array.length;
 }
